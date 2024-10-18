83a7d23129632105d802fc7800138972384ef136
Julia Dubina <166200647+JuliaDubin@users.noreply.github.comm>
2024-10-17 22:59:47 -0700

    modified

--- 8kyu/switch_it_up.js
+++ 8kyu/switch_it_up.js
@@ -43,20 +43,26 @@ function switchItUp(number){
           break;
     } 
   }
-    
-    
-  //Test case:
 
-  console.log(switchItUp(8), 'Eight')
-  console.log(switchItUp(5), 'Five')
-
-  //Other solutions:
-
-  switchItUp = n =>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
-  
   //
 
   function switchItUp(number) {
     return number == 0 ? 'Zero' : number == 1 ? 'One' : number == 2? 'Two' : number == 3 ? 'Three' : number == 4 ? 'Four' : number == 5 ? 'Five' :
     number == 6 ? 'Six' : number == 7 ? 'Seven' : number == 8 ? 'Eight' : number == 9 ? 'Nine' : NaN;
   }
+    
+
+  //Test case:
+
+  console.log(switchItUp(8), 'Eight')
+  console.log(switchItUp(5), 'Five')
+
+  
+
+  //Other solutions:
+
+  switchItUp = n =>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+  
+  
+
+  
